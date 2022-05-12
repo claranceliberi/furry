cb7f26c48f78dd03055c7e4b852d37103cd3a7e0
claranceliberi <liberintwari@gmail.com>
2022-05-12 10:42:11 +0200

    testing

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('furry.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from furry!');
+		vscode.window.showInformationMessage('Hello World from Vs code!');
 	});
 
 	context.subscriptions.push(disposable);
